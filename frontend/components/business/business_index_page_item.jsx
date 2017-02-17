66f931fb979ad391fd2ab2835ff22edb1bfa079f
Alex Liang <liangalex91@gmail.com>
2017-02-16 20:07:12 -0500

    Start Business Index Page Items

--- frontend/components/business/business_index_page_item.jsx
+++ frontend/components/business/business_index_page_item.jsx
@@ -4,13 +4,14 @@ const BusinessIndexPageItem = ({key, business, fetchBusiness}) => {
   return (
       <li>
         <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>name</li>
+          <li>street</li>
+          <li>city</li>
+          <li>state</li>
+          <li>zip</li>
+          <li>phone</li>
+          <li>latitude</li>
+          <li>longitude</li>
         </ul>
       </li>
   )
