9a3c0b464756b4397e4ac94fda18f52be66b23b5
Alex Liang <liangalex91@gmail.com>
2017-02-20 23:50:04 -0500

    Add html skeleton for business show

--- frontend/components/business/business_show_page.jsx
+++ frontend/components/business/business_show_page.jsx
@@ -13,53 +13,81 @@ class BusinessPageShow extends React.Component{
 
     return (
       <div className="business__show">
-        <div className="business__current-box">
-          <div className="business__current-review">
-            <li>{business.price}</li>
-            <li>{business.review_count}</li>
-            <li>{business.name}</li>
-          </div>
-          <ul className="business__current-info">
 
-            <li>{business.street}</li>
-            <li>{business.city}</li>
-            <li>{business.state}</li>
-            <li>{business.zip}</li>
-            <li>{business.phone}</li>
-          </ul>
+        <div className="business__box">
+          <div className="business__header-left">
+            <div className="business__name-box">
+              <span className="business__name">{business.name}</span>
+            </div>
+
+            <div className="business__review-box">
+              <span class="business__review-rating">
+                <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+              </span>
+              <span className="business__review-count">{business.review_count}</span>
+            </div>
+
+            <div className="business__price">
+                <span>{business.price}</span>
+            </div>
+          </div>
+
+          <div className="business__header-right">
+            <button>Write a Review</button>
+          </div>
         </div>
 
-        <div className="map">
-          <div className="map-img">
-            <span>{business.latitude}</span>
-            <span>{business.longitude}</span>
-          </div>
-          <div className="business__show-images">
+        <div className="business__map-img-box">
+          <div className="business__map">
             <img
               className="home__header-bar-images"
+              src={ window.assets.static_map }
+            />
+            <span>{business.latitude}</span>
+            <span>{business.longitude}</span>
+
+            <ul className="business__current-info">
+              <li>{business.street}</li>
+              <li>{business.city}</li>
+              <li>{business.state}</li>
+              <li>{business.zip}</li>
+              <li>{business.phone}</li>
+            </ul>
+          </div>
+
+          <div className="business__show-images">
+            <img
+              className="business__header-images"
               src={ window.assets.oysters }
               />
           </div>
           <img
-            className="home__header-bar-images"
+            className="business__header-images"
             src={ window.assets.steak }
             />
           <img
-            className="home__header-bar-images"
+            className="business__header-images"
             src={ window.assets.soup }
             />
         </div>
 
-        <div className="business__show-body">
+        <div className="business__show-reviews">
           <div className="reviews__container-box">
+
             <div className="reviews__container-form">
+              <form>
+                <span class="business__review-rating">
+                  <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+                </span>
+
+                <label>Your Review</label>
+                <textarea />
+              </form>
             </div>
 
             <div className="reviews__container-reviews">
             </div>
-          </div>
 
-          <div className="tags">
           </div>
         </div>
 
