92d948752b66986af1ffb4a1eacc6cd1e8c63ce9
Alex Liang <liangalex91@gmail.com>
2017-02-23 15:45:24 -0500

    Add map component

--- frontend/components/business/reviews/business_show_review_container.js
+++ frontend/components/business/reviews/business_show_review_container.js
@@ -1,21 +1,21 @@
-import { connect } from "react-redux";
-import { fetchReviews, deleteReview, editReview } from '../../actions/business_actions';
-import BusinessShowReviews from './business_show_reviews'
-const mapStateToProps = (state) => {
-  console.log("hitmapStatereview", state)
-  return ({
-    reviews: state.businesses.currentBusiness.reviews
-  })
-}
-
-const mapDispatchToProps = ( dispatch ) => {
-  return ({
-    fetchReviews: () => dispatch(fetchReviews()),
-    deleteReview: (review) => dispatch(deleteReview())
-  })
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(BusinessShowReviews);
+// import { connect } from "react-redux";
+// import { fetchReviews, deleteReview, editReview } from '../../actions/business_actions';
+// import BusinessShowReviews from './business_show_reviews'
+// const mapStateToProps = (state) => {
+//   console.log("hitmapStatereview", state)
+//   return ({
+//     reviews: state.businesses.currentBusiness.reviews
+//   })
+// }
+//
+// const mapDispatchToProps = ( dispatch ) => {
+//   return ({
+//     fetchReviews: () => dispatch(fetchReviews()),
+//     deleteReview: (review) => dispatch(deleteReview())
+//   })
+// }
+//
+// export default connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(BusinessShowReviews);
--- frontend/reducers/user_reducer.js
+++ frontend/reducers/user_reducer.js
@@ -1,8 +1,6 @@
 import { RECEIVE_USER } from '../actions/user_actions';
 import { merge } from "lodash";
 
-
-
 const UserReducer = (preloadedState = {}, action) => {
   Object.freeze(preloadedState)
   switch (action.type) {
