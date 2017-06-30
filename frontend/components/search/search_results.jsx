7b8242bd43863ff01e0f0fc10a6d251d9119390c
Alex Liang <liangalex91@gmail.com>
2017-06-29 23:44:56 -0400

    Add message for no search result found

--- frontend/components/search/search_results.jsx
+++ frontend/components/search/search_results.jsx
@@ -6,6 +6,7 @@ class SearchResults extends React.Component {
   constructor (props) {
     super(props)
     this.renderStars = this.renderStars.bind(this)
+    this.renderSearchResults = this.renderSearchResults.bind(this)
   }
 
   componentWillMount(){
@@ -29,43 +30,55 @@ class SearchResults extends React.Component {
     })
   }
 
-  render(){
-  return (
-    <div className="business__search-parent">
-      <div className="business__search-container">
-        <div className="business__search-index-box">
-          {
-            this.props.results.map( business => {
-              return (
-              <div key={`bizness-${business.id}`} className='business__search-item'>
-
-                <div className='business__search-left'>
-                <div className="business__search-index-item">
-                  <Link className="business__search-pic" to={`/businesses/${business.id}`}>{business.name}</Link>
-                  <div>{this.renderStars(business.ratings)}</div>
-                  <div>{business.review_count} Reviews</div>
-                  <div>{business.price}</div>
-                </div>
-                </div>
-                <ul className="business__search-right">
-                    <li>{business.street}</li>
-                    <li>{business.city}</li>
-                    <li>{business.state}, {business.zip}</li>
-                    <li>{business.phone}</li>
-                </ul>
-              </div>
-              )
-            }
-            )
-          }
+  renderSearchResults(props) {
+    console.log(this.props.location.query.query.length)
+    if(props.results.length < 1) {
+      return (
+        <div className='business__search-item-error'>
+          <div>{`No Results for ${props.location.query.query}`}</div>
+          <div>please try another keyword </div>
         </div>
-        <div className="business__search-map-box">
-          <BusinessMap
-            results={this.props.results}
-          />
+      )
+    } else {
+      return props.results.map( business => {
+          return (
+          <div key={`bizness-${business.id}`} className='business__search-item'>
+
+            <div className='business__search-left'>
+            <div className="business__search-index-item">
+              <Link className="business__search-pic" to={`/businesses/${business.id}`}>{business.name}</Link>
+              <div>{this.renderStars(business.ratings)}</div>
+              <div>{business.review_count} Reviews</div>
+              <div>{business.price}</div>
+            </div>
+            </div>
+            <ul className="business__search-right">
+                <li>{business.street}</li>
+                <li>{business.city}</li>
+                <li>{business.state}, {business.zip}</li>
+                <li>{business.phone}</li>
+            </ul>
+          </div>
+          )
+        }
+      )
+    }
+  }
+
+  render() {
+    return (
+      <div className="business__search-parent">
+        <div className="business__search-container">
+          <div className="business__search-index-box">
+            {this.renderSearchResults(this.props)}
+          </div>
+          <div className="business__search-map-box">
+            <BusinessMap
+              results={this.props.results}
+            />
+          </div>
         </div>
       </div>
-    </div>
     )
   }
 }
